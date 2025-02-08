bc8b8ca5bef241a8840276b732587270494a55f5
Marco Risi <marco.risi.dev@gmail.com>
2025-02-08 22:30:07 +0100

    Add domain type

---
+++ app/src/domain.ts
@@ -0,0 +1,4 @@
+export enum Who {
+    MOM = "mamma",
+    DAD = "papà"
+}
--- app/src/turnService.ts
+++ app/src/turnService.ts
@@ -1,5 +1,7 @@
+import { Who } from "./domain";
+
 export function getTurnMessage(date: Date): string {
   const day = date.getDate();
-  const who = day % 2 === 0 ? "mamma" : "papà";
+  const who = day % 4 === 0 ? Who.MOM : Who.DAD;
   return `Oggi tocca a... ${who}!`;
 }
