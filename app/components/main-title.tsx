4a2b6ef1e74143b4069f2f8e9c86234bbbaf8385
Marco Risi <marco.risi.dev@gmail.com>
2025-02-12 21:58:10 +0100

    Two lines for the title

--- app/components/main-title.tsx
+++ app/components/main-title.tsx
@@ -2,12 +2,11 @@ import { Text } from "react-native";
 import { Who } from "../src/domain";
 
 export function MainTitle({ who }: { who: Who }) {
-
-    const message = `Oggi tocca a... ${who}!`;
     
     return (
         <>
-            <Text>{message}</Text>
+            <Text>Oggi tocca ...</Text>
+            <Text>{who}!</Text>
         </>
     )
 }
