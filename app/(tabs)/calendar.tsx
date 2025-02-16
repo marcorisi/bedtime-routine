2cb3b82ba876f8f035c97f0847f194b6e20be52a
Marco Risi <marco.risi.dev@gmail.com>
2025-02-16 21:40:42 +0100

    Show a very simple calendar!

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,10 +1,42 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 
-export default function Calendar() {
+import { Calendar, LocaleConfig } from "react-native-calendars";
+
+export default function CalendarTab() {
+
+  LocaleConfig.locales['it'] = {
+    monthNames: [
+      'Gennaio',
+      'Febbraio',
+      'Marzo',
+      'Aprile',
+      'Maggio',
+      'Giugno',
+      'Luglio',
+      'Agosto',
+      'Settembre',
+      'Ottobre',
+      'Novembre',
+      'Dicembre'
+    ],
+    monthNamesShort: ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'],
+    dayNames: ['Domenica', 'Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato'],
+    dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab'],
+    today: "Oggi"
+  };
+  LocaleConfig.defaultLocale = 'it';
 
   return (
-    <View>
-      <Text>This is the calendar page...</Text>
+    <View style={styles.container}>
+      <Calendar />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+})
