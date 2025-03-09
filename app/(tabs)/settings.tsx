1aea6ceb76fd2b97b3b610c4ab46412e74a68192
Marco Risi <marco.risi.dev@gmail.com>
2025-03-09 17:32:37 +0100

    Draft form for the settings page

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,90 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, TextInput, Switch, StyleSheet, Button, Pressable } from "react-native";
 
 export default function Settings() {
+  const [numberOfDaysToConsider, setNumberOfDaysToConsider] = useState("30");
+  const [consecutiveDays, setConsecutiveDays] = useState("1");
+  const [isReversed, setIsReversed] = useState(false);
+
+  const handleSave = () => {
+    // Add your save logic here
+    console.log("Settings saved");
+  };
 
   return (
-    <View>
-      <Text>This is the settings page...</Text>
+    <View style={styles.container}>
+      <View style={styles.formContainer}>
+        <Text style={styles.label}>Giorni nel calendario:</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          value={numberOfDaysToConsider}
+          onChangeText={setNumberOfDaysToConsider}
+        />
+        <Text style={styles.label}>Giorni consecutivi:</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          value={consecutiveDays}
+          onChangeText={setConsecutiveDays}
+        />
+        <View style={styles.switchContainer}>
+          <Text style={styles.label}>Inverti ordine genitori?</Text>
+          <Switch
+            value={isReversed}
+            onValueChange={setIsReversed}
+          />
+        </View>
+        <Pressable style={styles.button} onPress={handleSave}>
+          <Text style={styles.buttonText}>Salva</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#fff5f7',
+  },
+  formContainer: {
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 24,
+    borderRadius: 16,
+    boxShadow: '0 0 8px rgba(0, 0, 0, 0.1)',
+  },
+  label: {
+    fontFamily: 'Bangers_400Regular',
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    fontFamily: 'Bangers_400Regular',
+    fontSize: 16,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 24,
+    paddingHorizontal: 16,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  button: {
+    backgroundColor: '#ed64a6',
+    padding: 16,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 24,
+  },
+  buttonText: {
+    fontFamily: 'Bangers_400Regular',
+    fontSize: 16,
+    color: 'white',
+  },
+});
