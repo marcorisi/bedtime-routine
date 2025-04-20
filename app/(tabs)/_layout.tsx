9c2242fc1672a7752c6b793a3eb186a54b1b4d58
Marco Risi <marco.risi.dev@gmail.com>
2025-04-20 23:53:24 +0200

    Use the color from the skin for the active tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,21 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
-import { Colors } from '../src/colors';
 import { featureFlags } from '../src/config';
+import AppSkinService from '../src/AppSkinService';
+import { useState } from 'react';
 
 export default function TabLayout() {
+  
+  const appSkinService = AppSkinService.getInstance();
+  const [color, setColor] = useState(appSkinService.getSkin().primaryTextColor);
+
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue', headerShown: false}}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: color, headerShown: false }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarActiveTintColor: Colors.primaryTextColor,
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={Colors.primaryTextColor} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
         }}
       />
       <Tabs.Screen
@@ -19,8 +23,7 @@ export default function TabLayout() {
         options={{
           title: 'Calendar',
           href: featureFlags.tabCalendar ? '/calendar' : null,
-          tabBarActiveTintColor: Colors.primaryTextColor,
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="calendar" color={Colors.primaryTextColor} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="calendar" color={color} />,
         }}
       />
       <Tabs.Screen
@@ -28,8 +31,7 @@ export default function TabLayout() {
         options={{
           title: 'Settings',
           href: featureFlags.tabSettings ? '/settings' : null,
-          tabBarActiveTintColor: Colors.primaryTextColor,
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={Colors.primaryTextColor} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
         }}
       />
     </Tabs>
